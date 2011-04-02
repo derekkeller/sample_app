5ac444fce33509e3e6ecb21d7a9e81e831de5c51
Derek Keller <derekbkeller@gmail.com>
2011-04-02 13:19:50 -0600

    Added spec/reqeusts/layout_links_spec

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,5 @@
 SampleApp::Application.routes.draw do
 
-  get "users/new"
   get "pages/about"
 
   root :to => 'pages#home'
---
+++ spec/requests/layout_links_spec.rb
@@ -0,0 +1,44 @@
+require 'spec_helper'
+
+describe "LayoutLinks" do
+  
+  it "should have a Home page at '/'" do
+    get '/'
+    response.should have_selector('title', :content => "Home")
+  end
+
+  it "should have a Contact page at '/contact'" do
+    get '/contact'
+    response.should have_selector('title', :content => "Contact")
+  end
+
+  it "should have an About page at '/about'" do
+    get'/about'
+    response.should have_selector('title', :content => "About")
+  end
+    
+  it "should have a Help page at '/help'" do
+    get '/help'
+    response.should have_selector('title', :content => "Help")
+  end
+
+  it "should have a signup page at '/signup'" do
+    get '/signup'
+    response.should have_selector('title', :content => "Sign Up")
+  end
+  
+  it "all the links should work on the layout" do
+    visit root_path
+    response.should have_selector('title', :content => "Home")
+    click_link "About"
+    response.should have_selector('title', :content => "About")
+    click_link "Contact"
+    response.should have_selector('title', :content => "Contact")
+    click_link "Help"
+    response.should have_selector('title', :content => "Help")
+    # click_link "Sign up now!"
+    # response.should have_selector('title', :content => "Sign Up")
+    response.should have_selector('a[href="/"]>img')
+  end
+  
+end
